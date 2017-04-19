4e4b9ae92519e4307bed15de3209e8bd1ab6985f
chriscarrillo <christopherscott.carrillo@calbaptist.edu>
2017-04-18 23:45:51 -0700

    HW8 Checkpoint 2 Commit

--- src/p2/sorts/QuickSort.java
+++ src/p2/sorts/QuickSort.java
@@ -2,58 +2,60 @@ package p2.sorts;
 
 import java.util.Comparator;
 
-import com.sun.xml.internal.bind.v2.model.annotation.Quick;
-import egr221a.exceptions.NotYetImplementedException;
-import egr221a.sorts.InsertionSort;
-
 public class QuickSort {
-
     public static <E extends Comparable<E>> void sort(E[] array) {
         QuickSort.sort(array, (x, y) -> x.compareTo(y));
     }
 
     public static <E> void sort(E[] array, Comparator<E> comparator) {
-        sort(array, 0, array.length - 1, comparator);
+        int left = 0;
+        int right = array.length - 1;
+        sort(array, left, right, comparator);
     }
 
-    private static <E> void sort(E[] a, int left, int right, Comparator<E> comparator) {
-        if (left <= right) {
-            E pivot = median3(a, left, right, comparator);
+    private static <E> void sort(E[] array, int left, int right, Comparator<E> comparator) {
+        if (left < right) {
+            int i = getMedian(array, left, right - 1, left + (right - 1 - left) / 2, comparator);
+            int front = left + 1;
+            int end = right;
 
-            // Begin partitioning
-            int i = left, j = right - 1;
-            for ( ; ; ) {
-                while (comparator.compare(a[++i], pivot) < 0) { }
-                while (comparator.compare(a[--j], pivot) > 0) { }
-                if (i < j)
-                    swap(a, i, j);
-                else
-                    break;
+            swap(array, left, i);
+            E pivot = array[left];
+
+            i = left;
+            while (front <= end) {
+                if (comparator.compare(array[front], pivot) >= 0) {
+                    swap(array, front, end);
+                    end--;
+                } else {
+                    swap(array, front, i);
+                    front++;
+                    i++;
+                }
             }
-
-            swap(a, i, right - 1); // Restore pivot
-
-            QuickSort.sort(a, left, i - 1, comparator);
-            QuickSort.sort(a, i + 1, right, comparator);
+            sort(array, left, i, comparator);
+            sort(array, i + 1, right, comparator);
         }
     }
 
-    private static <E> void swap(E[ ] a, int index1, int index2 ) {
-        E tmp = a[index1];
-        a[ index1 ] = a[ index2 ];
-        a[ index2 ] = tmp;
+    private static <E> int getMedian(E[] arr, int a, int b, int c, Comparator<E> comparator) {
+        if (verifyMedian(arr[a], arr[b], arr[c], comparator)) {
+            return b;
+        } else if (verifyMedian(arr[b], arr[a], arr[c], comparator)) {
+            return a;
+        } else {
+            return c;
+        }
     }
 
-    private static <E> E median3(E[ ] a, int left, int right, Comparator<E> comparator) {
-        int center = (left + right) / 2;
-        if (comparator.compare(a[center], a[left]) < 0)
-            swap(a, left, center);
-        if (comparator.compare(a[right], a[left]) < 0)
-            swap(a, left, right);
-        if (comparator.compare(a[right], a[center]) < 0)
-            swap(a, center, right);
+    private static <E> boolean verifyMedian(E a, E b, E c, Comparator<E> comparator) {
+        return (comparator.compare(a, b) < 0 && comparator.compare(b, c) < 0) ||
+                (comparator.compare(c, b) < 0 && comparator.compare(b, a) < 0);
+    }
 
-        swap(a, center, right - 1);
-        return a[right - 1];
+    private static <E> void swap(E[] array, int a, int b) {
+        E temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
